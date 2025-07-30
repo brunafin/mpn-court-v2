09745f85eea1f534e477444d2c92dc6fbae0ad9d
Bruna Nunes <bruna.f.n@hotmail.com>
2025-07-30 22:28:07 +0000

    ajuste build quebrado

--- src/contexts/NotificationContext.tsx
+++ src/contexts/NotificationContext.tsx
@@ -1,5 +1,5 @@
 // NotificationContext.tsx
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 interface NotificationContextProps {
     unreadCount: number;
